22fce65e795c774922f60de9d1c26fda1dac5a1e
Silvana <sdiazc9@upao.edu.pe>
2025-06-19 04:27:32 -0500

    Corrige CKV_AWS_338

--- main.tf
+++ main.tf
@@ -178,7 +178,7 @@ resource "aws_security_group" "lambda_sg" {
 
 resource "aws_cloudwatch_log_group" "apigw_logs" {
   name              = "/aws/apigateway/access-logs"
-  retention_in_days = 30
+  retention_in_days = 365
   kms_key_id        = aws_kms_key.lambda_env_key.arn
 }
 
