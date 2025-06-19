c374dd6175bea257a54e44959a0e6fd609f92f0f
CarlosMorales <cmoralesc7@upao.edu.pe>
2025-06-19 03:22:13 -0500

    Error 158 solucionado

--- main.tf
+++ main.tf
@@ -179,6 +179,7 @@ resource "aws_security_group" "lambda_sg" {
 resource "aws_cloudwatch_log_group" "apigw_logs" {
   name              = "/aws/apigateway/access-logs"
   retention_in_days = 30
+  kms_key_id        = aws_kms_key.lambda_env_key.arn
 }
 
 resource "aws_lambda_function" "lambdas" {
