0b58596d714e7fa591cd067452f4bb5050f47655
renzuk0 <renzolavado080205@gmail.com>
2025-06-18 21:42:58 -0500

    Corrección: Se agregó descripción a la regla de egress del SG (CKV_AWS_23)

--- main.tf
+++ main.tf
@@ -139,6 +139,7 @@ resource "aws_security_group" "lambda_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
   }
 }
 
